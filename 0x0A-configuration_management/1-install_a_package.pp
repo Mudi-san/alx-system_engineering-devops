27b8880a28157d3d3cb805890e5225c50d796be7
Mudi-san <mudijewilly@gmail.com>
2023-12-22 20:28:55 +0200

    Mudi is Done

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,10 @@
 #Install flask / Version must be 2.1.0
-package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3'
+package { 'pytho3-pip':
+  ensure   => 'installed',
+}
+exec {'install_flask':
+  command => '/usr/bin/pip3 install flask==2.1.0' ,
+  creates => '/usr/local/lib/pythin3.8/dist-packages/flask' ,
+  require => 
+Package['python3-pip'],
 }
